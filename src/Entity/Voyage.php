dfd6744ec6080c0e82e137b5c4e9b60ca5d0ba6a
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-16 09:24:11 +0100

    Add Voyage entity

---
+++ src/Entity/Voyage.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VoyageRepository")
+ */
+class Voyage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Jour", cascade={"persist"})
+     */
+    private $jours;
+
+    public function __construct()
+    {
+        $this->jours = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|Jour[]
+     */
+    public function getJours(): Collection
+    {
+        return $this->jours;
+    }
+
+    public function addJour(Jour $jour): self
+    {
+        if (!$this->jours->contains($jour)) {
+            $this->jours[] = $jour;
+        }
+
+        return $this;
+    }
+
+    public function removeJour(Jour $jour): self
+    {
+        if ($this->jours->contains($jour)) {
+            $this->jours->removeElement($jour);
+        }
+
+        return $this;
+    }
+}
