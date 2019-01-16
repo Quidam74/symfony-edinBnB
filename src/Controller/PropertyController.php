774ee71ade3d40f7ad38795bb0492e9f4b9c5551
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-16 13:04:13 +0100

    Add PropertyController

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/api/properties", methods={ "GET" })
+     */
+    public function listProperty() {
+        $repository = $this->getDoctrine()->getRepository(Property::class);
+
+        $properties = $repository->findAll();
+
+        $serializer = $this->container->get('serializer');
+        $reports = $serializer->serialize($properties, 'json');
+
+        return new Response($reports);
+    }
+
+    /**
+     * @Route("/api/properties", methods={ "POST" })
+     */
+    public function createProperty() {
+        return "Not implemented yet.";
+    }
+
+    /**
+     * @Route("/api/properties/{propertiesId}", methods={ "GET" }, requirements={"propertiesId"="\d+"})
+     */
+    public function readProperty($propertiesId) {
+        return "Not implemented yet.";
+    }
+
+    /**
+     * @Route("/api/properties/{propertiesId}", methods={ "PUT" }, requirements={"propertiesId"="\d+"})
+     */
+    public function updateProperty($propertiesId) {
+        return "Not implemented yet.";
+    }
+
+    /**
+     * @Route("/api/properties/{propertiesId}", methods={ "DELETE" }, requirements={"propertiesId"="\d+"})
+     */
+    public function deleteProperty($propertiesId) {
+        return "Not implemented yet.";
+    }
+}
