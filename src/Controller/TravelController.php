d8a169c29bf90c13c786b7aa8377a8ec778faaf8
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-16 13:08:12 +0100

    Add TravelController

---
+++ src/Controller/TravelController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Travel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TravelController extends AbstractController
+{
+    /**
+     * @Route("/api/travels", methods={ "GET" })
+     */
+    public function listTravel() {
+        $repository = $this->getDoctrine()->getRepository(Travel::class);
+
+        $travel = $repository->findAll();
+
+        $serializer = $this->container->get('serializer');
+        $reports = $serializer->serialize($travel, 'json');
+
+        return new Response($reports);
+    }
+
+    /**
+     * @Route("/api/travels", methods={ "POST" })
+     */
+    public function createTravel() {
+        return "Not implemented yet.";
+    }
+
+    /**
+     * @Route("/api/travel/{travelId}", methods={ "GET" }, requirements={"travelId"="\d+"})
+     */
+    public function readTravel($travelId) {
+        return "Not implemented yet.";
+    }
+
+    /**
+     * @Route("/api/travel/{travelId}", methods={ "PUT" }, requirements={"travelId"="\d+"})
+     */
+    public function updateTravel($travelId) {
+        return "Not implemented yet.";
+    }
+
+    /**
+     * @Route("/api/travel/{travelId}", methods={ "DELETE" }, requirements={"travelId"="\d+"})
+     */
+    public function deleteTravel($travelId) {
+        return "Not implemented yet.";
+    }
+}
