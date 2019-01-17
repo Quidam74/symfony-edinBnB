199ee9fc9dad83c044aaa0e0c257a8e53b15b373
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-17 08:55:47 +0100

    Implement createUser

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Api;
 
 use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,8 +29,34 @@ class UserController extends AbstractController
     /**
      * @Route("/api/users", methods={ "POST" })
      */
-    public function createUser() {
-        return "Not implemented yet.";
+    public function createUser(Request $request) {
+        $user = new User();
+
+        $manager = $this->getDoctrine()->getManager();
+        // Get the data of request.
+        $data = json_decode($request->getContent(), true);
+
+        // Create form without csrf protection.
+        $form = $this->createForm(UserType::class, $user, array("csrf_protection" => false));
+        $form->handleRequest($request)
+            ->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // If the request is valide, save the new User.
+            $manager->persist($user);
+            $manager->flush();
+
+            $serializer = $this->container->get('serializer');
+            $reports = $serializer->serialize($user, 'json');
+
+            // Return the created User.
+            return new Response($reports);
+        }
+
+        // Else return an error.
+        $response = new JsonResponse(array("message" => "Attribute(s) missing !"));
+        $response->setStatusCode(400);
+        return $response;
     }
 
     /**
