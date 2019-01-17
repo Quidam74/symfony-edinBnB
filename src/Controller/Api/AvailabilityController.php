3c386d50dcff5d9589558365ae6f1366d217c8c1
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-17 07:32:00 +0100

    Implement createAvailability and readAvailability

--- src/Controller/Api/AvailabilityController.php
+++ src/Controller/Api/AvailabilityController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Api;
 
 use App\Entity\Availability;
+use App\Form\AvailabilityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,6 +20,7 @@ class AvailabilityController extends AbstractController
 
         $availabilities = $repository->findAll();
 
+        // Parse Object to jsonString.
         $serializer = $this->container->get('serializer');
         $reports = $serializer->serialize($availabilities, 'json');
 
@@ -26,15 +30,50 @@ class AvailabilityController extends AbstractController
     /**
      * @Route("/api/availabilities", methods={ "POST" })
      */
-    public function createAvailability() {
-        return "Not implemented yet.";
+    public function createAvailability(Request $request) {
+        $availability = new Availability();
+
+        $manager = $this->getDoctrine()->getManager();
+        // Get the data of request.
+        $data = json_decode($request->getContent(), true);
+
+        // Create form without csrf protection.
+        $form = $this->createForm(AvailabilityType::class, $availability, array("csrf_protection" => false));
+        $form->handleRequest($request)
+            ->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // If the request is valide, save the new Availability.
+            $manager->persist($availability);
+            $manager->flush();
+
+            $serializer = $this->container->get('serializer');
+            $reports = $serializer->serialize($availability, 'json');
+
+            // Return the created Address.
+            return new Response($reports);
+        }
+
+        // Else return an error.
+        $response = new JsonResponse(array("message" => "Attribute(s) missing !"));
+        $response->setStatusCode(400);
+        return $response;
     }
 
     /**
      * @Route("/api/availability/{availabilityId}", methods={ "GET" }, requirements={"availabilityId"="\d+"})
      */
     public function readAvailability($availabilityId) {
-        return "Not implemented yet.";
+        $repository = $this->getDoctrine()->getRepository(Availability::class);
+
+        // Find the Availability with id $availabilityId.
+        $address = $repository->find($availabilityId);
+
+        // Parse Object to jsonString.
+        $serializer = $this->container->get('serializer');
+        $reports = $serializer->serialize($address, 'json');
+
+        return new Response($reports);
     }
 
     /**
