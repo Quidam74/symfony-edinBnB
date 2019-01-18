6c4a9524cf2779164136080281d97a44fdcac6c6
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-18 15:08:37 +0100

    Use ApiRESTController for EquipmentController

--- src/Controller/Api/AddressController.php
+++ src/Controller/Api/AddressController.php
@@ -4,11 +4,8 @@ namespace App\Controller\Api;
 
 use App\Entity\Address;
 use App\Form\AddressType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 class AddressController extends ApiRESTController
 {
--- src/Controller/Api/EquipmentController.php
+++ src/Controller/Api/EquipmentController.php
@@ -4,29 +4,17 @@ namespace App\Controller\Api;
 
 use App\Entity\Equipment;
 use App\Form\EquipmentType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
 
-class EquipmentController extends AbstractController
+class EquipmentController extends ApiRESTController
 {
     /**
      * @Route("/api/equipments", methods={ "GET" })
      */
     public function listEquipment()
     {
-        $repository = $this->getDoctrine()->getRepository(Equipment::class);
-
-        $equipments = $repository->findAll();
-
-        $serializer = $this->container->get('serializer');
-        $reports = $serializer->serialize($equipments, 'json');
-
-        $response = new Response($reports);
-        $response->headers->set("Content-Type", "application/json");
-        return $response;
+        return $this->list(Equipment::class);
     }
 
     /**
@@ -34,35 +22,7 @@ class EquipmentController extends AbstractController
      */
     public function createEquipment(Request $request)
     {
-        $equipment = new Equipment();
-
-        $manager = $this->getDoctrine()->getManager();
-        // Get the data of request.
-        $data = json_decode($request->getContent(), true);
-
-        // Create form without csrf protection.
-        $form = $this->createForm(EquipmentType::class, $equipment, array("csrf_protection" => false));
-        $form->handleRequest($request)
-            ->submit($data);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // If the request is valide, save the new Equipment.
-            $manager->persist($equipment);
-            $manager->flush();
-
-            $serializer = $this->container->get('serializer');
-            $reports = $serializer->serialize($equipment, 'json');
-
-            // Return the created Equipment.
-            $response = new Response($reports);
-            $response->headers->set("Content-Type", "application/json");
-            return $response;
-        }
-
-        // Else return an error.
-        $response = new JsonResponse(array("message" => "Attribute(s) missing !"));
-        $response->setStatusCode(400);
-        return $response;
+        return $this->create($request, EquipmentType::class, Equipment::class);
     }
 
     /**
@@ -70,18 +30,7 @@ class EquipmentController extends AbstractController
      */
     public function readEquipment($equipmentId)
     {
-        $repository = $this->getDoctrine()->getRepository(Equipment::class);
-
-        // Find the Equipment with id $equipmentId.
-        $equipment = $repository->find($equipmentId);
-
-        // Parse Object to jsonString.
-        $serializer = $this->container->get('serializer');
-        $reports = $serializer->serialize($equipment, 'json');
-
-        $response = new Response($reports);
-        $response->headers->set("Content-Type", "application/json");
-        return $response;
+        return $this->read(Equipment::class, $equipmentId);
     }
 
     /**
@@ -89,38 +38,7 @@ class EquipmentController extends AbstractController
      */
     public function updateEquipment(Request $request, $equipmentId)
     {
-        $repository = $this->getDoctrine()->getRepository(Equipment::class);
-        // Find the Equipment with id $equipmentId.
-        $equipment = $repository->find($equipmentId);
-
-        $manager = $this->getDoctrine()->getManager();
-        // Get the data of request.
-        $data = json_decode($request->getContent(), true);
-
-        $form = $this->createForm(EquipmentType::class, $equipment, array("csrf_protection" => false));
-        $form->handleRequest($request)
-            ->submit($data);
-
-        // Create form without csrf protection.
-        if ($form->isSubmitted() && $form->isValid()) {
-            // If the request is valide, save the new Equipment.
-            $manager->persist($equipment);
-            $manager->flush();
-
-            // Parse Object to jsonString.
-            $serializer = $this->container->get('serializer');
-            $reports = $serializer->serialize($equipment, 'json');
-
-            // Return the created Equipment.
-            $response = new Response($reports);
-            $response->headers->set("Content-Type", "application/json");
-            return $response;
-        }
-
-        // Else return an error.
-        $response = new JsonResponse(array("message" => "Attribute(s) missing !"));
-        $response->setStatusCode(400);
-        return $response;
+        return $this->update($request, EquipmentType::class, Equipment::class, $equipmentId);
     }
 
     /**
@@ -128,15 +46,6 @@ class EquipmentController extends AbstractController
      */
     public function deleteEquipment($equipmentId)
     {
-        $manager = $this->getDoctrine()->getManager();
-        $repository = $this->getDoctrine()->getRepository(Equipment::class);
-        // Find the Equipment with id $equipmentId.
-        $equipment = $repository->find($equipmentId);
-
-        // Remove the Equipment.
-        $manager->remove($equipment);
-        $manager->flush();
-
-        return new JsonResponse(array("message" => "Successfully deleted."));
+        return $this->delete(Equipment::class, $equipmentId);
     }
 }
