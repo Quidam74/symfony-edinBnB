83afa412b14fa8986605e66a66cad96ebd957faf
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-18 15:16:44 +0100

    Use ApiRESTController for UserController

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -4,29 +4,17 @@ namespace App\Controller\Api;
 
 use App\Entity\User;
 use App\Form\UserType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserController extends AbstractController
+class UserController extends ApiRESTController
 {
     /**
      * @Route("/api/users", methods={ "GET" })
      */
     public function listUsers()
     {
-        $repository = $this->getDoctrine()->getRepository(User::class);
-
-        $users = $repository->findAll();
-
-        $serializer = $this->container->get('serializer');
-        $reports = $serializer->serialize($users, 'json', ['groups' => 'user']);
-
-        $response = new Response($reports);
-        $response->headers->set("Content-Type", "application/json");
-        return $response;
+        return $this->list(User::class, ['user']);
     }
 
     /**
@@ -34,35 +22,7 @@ class UserController extends AbstractController
      */
     public function createUser(Request $request)
     {
-        $user = new User();
-
-        $manager = $this->getDoctrine()->getManager();
-        // Get the data of request.
-        $data = json_decode($request->getContent(), true);
-
-        // Create form without csrf protection.
-        $form = $this->createForm(UserType::class, $user, array("csrf_protection" => false));
-        $form->handleRequest($request)
-            ->submit($data);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // If the request is valide, save the new User.
-            $manager->persist($user);
-            $manager->flush();
-
-            $serializer = $this->container->get('serializer');
-            $reports = $serializer->serialize($user, 'json', ['groups' => 'user']);
-
-            // Return the created User.
-            $response = new Response($reports);
-            $response->headers->set("Content-Type", "application/json");
-            return $response;
-        }
-
-        // Else return an error.
-        $response = new JsonResponse(array("message" => "Attribute(s) missing !"));
-        $response->setStatusCode(400);
-        return $response;
+        return $this->create($request, UserType::class, User::class, ['user']);
     }
 
     /**
@@ -70,18 +30,7 @@ class UserController extends AbstractController
      */
     public function readUser($userId)
     {
-        $repository = $this->getDoctrine()->getRepository(User::class);
-
-        // Find the User with id $userId.
-        $user = $repository->find($userId);
-
-        // Parse Object to jsonString.
-        $serializer = $this->container->get('serializer');
-        $reports = $serializer->serialize($user, 'json', ['groups' => 'user']);
-
-        $response = new Response($reports);
-        $response->headers->set("Content-Type", "application/json");
-        return $response;
+        return $this->read(User::class, $userId, ['user']);
     }
 
     /**
@@ -89,38 +38,7 @@ class UserController extends AbstractController
      */
     public function updateUser(Request $request, $userId)
     {
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        // Find the User with id $userId.
-        $user = $repository->find($userId);
-
-        $manager = $this->getDoctrine()->getManager();
-        // Get the data of request.
-        $data = json_decode($request->getContent(), true);
-
-        $form = $this->createForm(UserType::class, $user, array("csrf_protection" => false));
-        $form->handleRequest($request)
-            ->submit($data);
-
-        // Create form without csrf protection.
-        if ($form->isSubmitted() && $form->isValid()) {
-            // If the request is valide, save the new User.
-            $manager->persist($user);
-            $manager->flush();
-
-            // Parse Object to jsonString.
-            $serializer = $this->container->get('serializer');
-            $reports = $serializer->serialize($user, 'json', ['groups' => 'user']);
-
-            // Return the created User.
-            $response = new Response($reports);
-            $response->headers->set("Content-Type", "application/json");
-            return $response;
-        }
-
-        // Else return an error.
-        $response = new JsonResponse(array("message" => "Attribute(s) missing !"));
-        $response->setStatusCode(400);
-        return $response;
+        return $this->update($request, UserType::class, User::class, $userId, ['user']);
     }
 
     /**
@@ -128,15 +46,6 @@ class UserController extends AbstractController
      */
     public function deleteUser($userId)
     {
-        $manager = $this->getDoctrine()->getManager();
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        // Find the User with id $userId.
-        $user = $repository->find($userId);
-
-        // Remove the User.
-        $manager->remove($user);
-        $manager->flush();
-
-        return new JsonResponse(array("message" => "Successfully deleted."));
+        return $this->delete(User::class, $userId);
     }
 }
